9733b63fabda07f8c11820da001bfe291007c545
mzg <15122302865@163.com>
2018-04-29 08:30:39 +0800

    完成数据分析及可视化..

--- challenge7_3.py
+++ challenge7_3.py
@@ -2,11 +2,10 @@
 
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib import ticker
 
 def climate_plot():
     # 1.查看数据文件结构
-    # 直接读取 NASA 全球温度变化数据集
-    df_temperature = pd.read_excel('GlobalTemperature.xlsx')
 
     # 传入世界银行气候变化数据集
     df_climate = pd.read_excel("ClimateChange.xlsx",sheetname='Data')
@@ -17,41 +16,82 @@ def climate_plot():
     df_nox = df_climate[df_climate['Series code']=='EN.ATM.NOXE.KT.CE']
     df_ghgo = df_climate[df_climate['Series code']=='EN.ATM.GHGO.KT.CE']
     df_ghgr = df_climate[df_climate['Series code']=='EN.CLC.GHGR.MT.CE']
-    
+    # 组合读取到的数据
     df_ghg_concat = pd.concat([df_co2,df_met,df_nox,df_ghgo,df_ghgr],axis=0)
     df_ghg_drop = df_ghg_concat.drop(['Country name','Series code','Series name','SCALE','Decimals'],axis=1).set_index('Country code').replace({'..':pd.np.NaN})
-    
+    # 填充
     df_ghg_fill = df_ghg_drop.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1).dropna(how='all')
-
+    
+    # 求和
     df_ghg_fill.loc['total'] = df_ghg_fill.apply(lambda x:x.sum(),axis=0)
     # 矩阵转置
     df_ghg_zz = df_ghg_fill.loc['total'].T
 
-    xmax = df_ghg_zz.max()
-    xmin = df_ghg_zz.min()
-    df_ghg_nor = df_ghg_zz.apply(lambda x:round((x-xmin)/(xmax-xmin),3))
-    print(df_ghg_nor)
-    #df_dic = {'df_dic':df_co2,'df_met':df_met,'df_nox':df_nox,'df_ghgo':df_ghgo,'df_ghgr':df_ghgr}
-    # 归一化后的数据存入该字典
-    #df_handled_list = []
     
-#    for df_k,df_v in df_dic.items():
-#        # 去掉不相关数据列
-#        df_v_drop = df_v.drop(['Country name','Series code','Series name','SCALE','Decimals'],axis=1).set_index('Country code').replace({'..':pd.np.NaN})
-#        # 数据填充
-#        df_v_fill = df_v_drop.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1).dropna(how='all')
-#
-#        #数据归一化
-#        df_v_nor = df_v_fill.apply(lambda x:round((x-x.min())/(x.max()-x.min()),3))
-#        # 存入列表
-#        df_handled_list.append(df_v_nor)
-#    df_concat = pd.concat(df_handled_list,axis=0)
-    #print(df_concat)
+    # 直接读取 NASA 全球温度变化数据集
+    df_temper = pd.read_excel('GlobalTemperature.xlsx')
+    df_temper['Date'] = pd.to_datetime(df_temper['Date'])
+    df_temper_date = df_temper.set_index('Date')
+    # 选取1990-2011年间的温度变化数据
+    df_temper_r = df_temper_date[['Land Average Temperature','Land And Ocean Average Temperature']]
+    # 按照 A－年，Q－季度 重采样
+    df_temper_ra = df_temper_r.resample('A').mean()
+    df_temper_rq = df_temper_r.resample('Q').mean()
+    
+    # 选取1990～2010年的数据
+    df_temper_ra_slice = df_temper_ra['1990-1-1':'2010-12-31']
+    
+    # 合并从两个数据集读取到的数据
+    df_merge_temp = pd.concat([df_temper_ra_slice.reset_index(),df_ghg_zz.loc['1990':'2010'].reset_index()],axis=1)
+    df_merge = df_merge_temp.drop(['Date'],axis=1).set_index('index')
+    
+    # 数据归一化
+    df_nor = (df_merge-df_merge.min())/(df_merge.max()-df_merge.min())
+    print(df_nor)
+    
+
+    # 绘图4*4
+    fig,axes = plt.subplots(nrows=2,ncols=2)
+    
+    # 子图1
+    ax1 = df_nor.plot(
+            kind='line',
+            figsize=(16,9),
+            ax=axes[0,0],
+            )
+    
+    ax1.set_xlabel('Years')
+    ax1.set_ylabel('Values')
+    ax1.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+    # 子图2
+    ax2 = df_nor.plot(
+            kind='bar',
+            figsize=(16,9),
+            ax=axes[0,1],
+            )
+    ax2.set_xlabel('Years')
+    ax2.set_ylabel('Values')
+    # 子图3
+    ax3 = df_temper_rq.plot(
+            kind='area',
+            figsize=(16,9),
+            ax=axes[1,0],
+            )
+    ax3.set_xlabel('Quarters')
+    ax3.set_ylabel('Temperature')
+    # 子图4
+    ax4 = df_temper_rq.plot(
+            kind='kde',
+            figsize=(16,9),
+            ax=axes[1,1],
+            )
+    ax4.set_xlabel('Values')
+    ax4.set_ylabel('Values')
+
+    plt.show()
+
     # 3.对时间序列数据集进行处理并重新采样
     
-    #print(df_temperature.head(20))
-    #df_temperature['Date'] = pd.to_datetime(df_temperature['Date'])
-    #df_temperature
     # 4.按规定选择数据
     # 5.按规定绘图
 
@@ -59,7 +99,7 @@ def climate_plot():
     #fig = plt.subplot()
 
     # 返回 fig 对象
-    #return fig
+    return fig
 
 if __name__=='__main__':
     climate_plot()
